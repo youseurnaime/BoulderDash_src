1f1c0936f1de78d454d4cae2cb8eba1197cb790d
dekefake <desathomas@gmail.com>
2017-02-07 09:10:21 +0100

    Ebauche passage niveaux en args

--- boulderTest/Main.java
+++ boulderTest/Main.java
@@ -3,9 +3,32 @@ package boulderTest;
 public class Main {
 
 	public static void main(String[] args) {
+		String fichierNiveau="niveaux.bdcff";
+		if(args==null){
+			// No arguments
+		} else {
+			switch (args.length){
+			case 1:
+				switch (args[0]){
+				case "-name":
+					// arg -name
+					break;
+				case "-h" :
+					// arg -h
+					break;
+				}
+				break;
+			case 2:
+				switch (args[0]){
+					case "-lis":
+						fichierNiveau=args[1];
+					}
+				break;
+			}
+		}
 		System.out.println("Bonjour");
 		try{
-			EnsembleNiveau lesNiveaux = new EnsembleNiveau("niveaux.bdcff");
+			EnsembleNiveau lesNiveaux = new EnsembleNiveau(fichierNiveau);
 			System.out.println(lesNiveaux.toString());
 			lesNiveaux.choisirNiveau();
 		}catch(Exception e){
