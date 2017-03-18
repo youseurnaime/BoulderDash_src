ec086af9076a49558d1e0c02419b9d64aa10ac2f
marin <marin.fabre@hotmail.fr>
2017-03-18 16:39:39 +0100

    IA simplet ok

--- boulderTest/Simplet.java
+++ boulderTest/Simplet.java
@@ -8,6 +8,29 @@ import java.awt.*;
 public class Simplet extends Rockford {
 
     public Point getDeplacement(Point posRockford, Map laMap) {
-        return null;
+        int choixRandom; // Int random entre 0 et 5
+        Point essaieDeDeplacement;
+        do{
+            choixRandom = (int) (Math.random() * 6); // Int random entre 0 et 5
+            switch (choixRandom){
+                case 1:
+                    essaieDeDeplacement = new Point((int) posRockford.getX() + 1, (int) posRockford.getY());
+                    break;
+                case 2:
+                    essaieDeDeplacement = new Point((int) posRockford.getX() - 1, (int) posRockford.getY());
+                    break;
+                case 3:
+                    essaieDeDeplacement = new Point((int) posRockford.getX(), (int) posRockford.getY() + 1);
+                    break;
+                case 4:
+                    essaieDeDeplacement = new Point((int) posRockford.getX(), (int) posRockford.getY() - 1);
+                    break;
+                case 0:
+                default:
+                    essaieDeDeplacement = posRockford;
+                    break;
+            }
+        }while(!deplacementPossible(essaieDeDeplacement,posRockford,laMap));
+        return essaieDeDeplacement;
     }
 }
